655aebf8aa70c36a9f2a8ef07f4fdcada99ceaf4
muzimusi <arjenlee1992@gmail.com>
2020-10-13 00:40:20 +0800

    TwoSum

--- src/main/java/array/TwoSum.java
+++ src/main/java/array/TwoSum.java
@@ -2,12 +2,23 @@ package array;
 
 import java.util.HashMap;
 
-/*
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-*/
+/**
+ * leetcode 1 E
+ * *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * *
+ * 示例:
+ * *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ */
 public class TwoSum {
     //    暴力解法
     public int[] twoSum03(int[] nums, int target) {
